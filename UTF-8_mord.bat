f0035b6c4a7918d72b933a2911066516d2e87c6d
Valkyrja <[valkyrja45queen@outlook.jp]>
2023-10-15 11:15:05 +0900

    save

---
+++ UTF-8_mord.bat
@@ -0,0 +1,27 @@
+@echo off
+::chcp 65001 > nul
+chcp 65001
+:: コマンドプロンプトをUTF-8モードで開く
+:: cmd.exe /u /k
+cmd
+:: .exeファイルの一覧を取得
+setlocal enabledelayedexpansion
+set "count=0"
+for %%i in (*.exe) do (
+    set /a count+=1
+    set "exe[!count!]=%%i"
+    echo !count!. %%i
+)
+
+:: ユーザーの選択を受け付ける
+set /p choice=実行する.exeファイルを選択してください (1から%count%までの番号を入力): 
+
+:: ユーザーの選択に基づいて.exeファイルを実行
+if "%choice%" geq "1" if "%choice%" leq "%count%" (
+    start "!exe[%choice%]!"
+) else (
+    echo 無効な選択です。
+)
+
+:: この行まで到達すると、コマンドプロンプトを閉じる
+exit
